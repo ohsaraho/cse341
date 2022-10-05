7399082b59d7f74d64792605844053f613531b6e
ohsaraho <osg21001@byui.edu>
2022-10-04 23:26:23 -0600

    add code for react website

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -2,18 +2,26 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAllIDs = async (req, res) => {
-  const result = await mongodb.getDb().db('cse341_projects').collection('contacts').find();
-  result.toArray().then((documents) => {
-    res.json(documents);
-  });
+  try {
+    const result = await mongodb.getDb().db('cse341_projects').collection('contacts').find();
+    result.toArray().then((documents) => {
+      res.json(documents);
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const getSingleID = async (req, res) => {
-  const documentId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db('cse341_projects').collection('contacts').find({ _id: documentId });
-  result.toArray().then((document) => {
-    res.json(document[0])
-  });
+  try {
+    const documentId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().db('cse341_projects').collection('contacts').find({ _id: documentId });
+    result.toArray().then((document) => {
+      res.json(document[0])
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 };
 
 const createNewContact = async (req, res) => {
@@ -25,60 +33,72 @@ const createNewContact = async (req, res) => {
   //     birthday: "1968-04-10T07:00:00.000+00:00"
 
   // };
-  const contact = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
+  try {
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
 
-  };
+    };
 
-  const response = await mongodb.getDb().db('cse341_projects').collection('contacts').insertOne(contact);
+    const response = await mongodb.getDb().db('cse341_projects').collection('contacts').insertOne(contact);
 
-  if (response.acknowledged) {
-    res.status(201).json(response);
-  } else {
-    res.status(500).json(response.error || 'An error occurred while trying to create the contact.');
+    if (response.acknowledged) {
+      res.status(201).json(response);
+    } else {
+      res.status(500).json(response.error || 'An error occurred while trying to create the contact.');
+    }
+  } catch (err) {
+    res.status(500).json(err);
   }
 };
 
 const updateContact = async (req, res) => {
-  const documentId = new ObjectId(req.params.id);
-  const updateContactDoc = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
+  try {
+    const documentId = new ObjectId(req.params.id);
+    const updateContactDoc = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
 
-  };
-  
-  // Changes just the favoriteColor // Use updateOne because it keeps the data that is already there and updates the new fields but replaceOne replaces the whole document
-  // The $set operator allows you to replace a field that you specified with that value
-  // let updateContactDoc = {
-  //   $set: { favoriteColor: "Green" }
-  // };
+    };
+    
+    // Changes just the favoriteColor // Use updateOne because it keeps the data that is already there and updates the new fields but replaceOne replaces the whole document
+    // The $set operator allows you to replace a field that you specified with that value
+    // let updateContactDoc = {
+    //   $set: { favoriteColor: "Green" }
+    // };
 
-  const response = await mongodb.getDb().db('cse341_projects').collection('contacts').replaceOne({ _id: documentId }, updateContactDoc);
+    const response = await mongodb.getDb().db('cse341_projects').collection('contacts').replaceOne({ _id: documentId }, updateContactDoc);
 
-  if (response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'An error occurred while trying to update the contact.');
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'An error occurred while trying to update the contact.');
+    }
+  } catch (err) {
+    res.status(500).json(err);
   }
 };
 
 
 const deleteContact = async (req, res) => {
-  const documentId = new ObjectId(req.params.id);
+  try {
+    const documentId = new ObjectId(req.params.id);
 
-  const response = await mongodb.getDb().db('cse341_projects').collection('contacts').deleteOne({ _id: documentId }, true);
+    const response = await mongodb.getDb().db('cse341_projects').collection('contacts').deleteOne({ _id: documentId }, true);
 
-  if (response.deletedCount > 0) {
-    res.status(200).send();
-  } else {
-    res.status(500).json(response.error || 'An error occurred while trying to delete the contact.');
+    if (response.deletedCount > 0) {
+      res.status(200).send();
+    } else {
+      res.status(500).json(response.error || 'An error occurred while trying to delete the contact.');
+    }
+  } catch (err) {
+    res.status(500).json(err);
   }
 };
 
