7a506083ebdc6a9f38578737b2bd847a5f60bcaa
ohsaraho <osg21001@byui.edu>
2022-10-06 22:43:38 -0600

    took out content-type json

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
-    res.setHeader('Content-Type', 'application/json');
+    // res.setHeader('Content-Type', 'text/html');
+    // res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
